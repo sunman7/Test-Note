69b75d20074468f499ceca95e6c9ad8c04cb22d2
sunman7 <473910527@qq.com>
2020-12-05 14:25:01 +0800

    修复

--- Sort/quickSort.js
+++ Sort/quickSort.js
@@ -1,12 +1,11 @@
 let quickSort = (arr) =>{
-    let k = arr.length;
-    if(k <= 1) return arr;
+    if(arr.length <= 1) return arr;
+    let index = Math.floor(arr.length / 2);
+    let pivot = arr.splice(index,1)[0];
     let left = [];
     let right = [];
-    let index = Math.floor( k/2 );
-    let pivot = arr.splice(index,1)[0]; 
-    for(let i = 0 ;i<k;i++){
-        if(pivot < arr[i]){
+    for(let i =0;i<arr.length ;i++){
+        if(arr[i] < pivot){
             left.push(arr[i]);
         }else{
             right.push(arr[i]);
