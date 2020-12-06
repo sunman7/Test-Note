6721713849ca5f9e82ae83436cfbeefea5a0e08d
sunman7 <473910527@qq.com>
2020-12-06 22:30:55 +0800

    version 2

--- dom-2/src/jquery.js
+++ dom-2/src/jquery.js
@@ -7,7 +7,7 @@ window.jQuery = function (selectorOrArray) {
   }
 
   return {
-    oldJQ: selectorOrArray.oldJQ,
+    oldJQ: elements.oldJQ,
     addClass(className) {
       for (let i = 0; i < elements.length; i++) {
         elements[i].classList.add(className);
