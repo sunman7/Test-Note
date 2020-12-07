f0f9ae645b85b785b6a0d0b7d4b955c6367c7ad6
sunman7 <473910527@qq.com>
2020-12-07 11:59:40 +0800

    添加原型

--- dom-2/src/jquery.js
+++ dom-2/src/jquery.js
@@ -1,4 +1,4 @@
-window.jQuery = function (selectorOrArray) {
+window.$ = window.jQuery = function (selectorOrArray) {
   let elements;
   if (typeof selectorOrArray === "string") {
     elements = document.querySelectorAll(selectorOrArray);
@@ -6,68 +6,82 @@ window.jQuery = function (selectorOrArray) {
     elements = selectorOrArray;
   }
 
-  return {
+  const api = Object.create(jQuery.prototype);
+  //创建一个对象，对象的__proto__为JQuery.prototype;
+  Object.assign(api, {
+    elements: elements,
     oldJQ: elements.oldJQ,
-    addClass(className) {
-      for (let i = 0; i < elements.length; i++) {
-        elements[i].classList.add(className);
+  });
+  // api.elements = elements;
+  // api.oldJQ = elements.oldJQ;
+  return api;
+};
+jQuery.fn = jQuery.prototype = {
+  jQuery: true,
+  constructor: jQuery,
+
+  get(index) {
+    return this.elements[index];
+  },
+  addClass(className) {
+    for (let i = 0; i < this.elements.length; i++) {
+      this.elements[i].classList.add(className);
+    }
+    return this;
+    // const api  = {....}
+    //return api;
+  },
+  find(selector) {
+    let arr = [];
+    for (let i = 0; i < this.elements.length; i++) {
+      arr = arr.concat(Array.from(this.elements[i].querySelectorAll(selector)));
+    }
+    arr.oldJQ = this;
+    //保存执行find操作的JQ对象（旧JQ对象）
+    return jQuery(arr);
+    //这里创建一个新的jQuery对象，因为不能跟执行find操作的jQuery对象是一样的
+    //如果不返回新的jQuery对象，可能会
+  },
+  each(fn) {
+    for (let i = 0; i < this.elements.length; i++) {
+      fn.call(null, this.elements[i], i);
+    }
+    return this;
+  },
+  parent() {
+    const arr = [];
+    this.each((node) => {
+      if (arr.indexOf(node.parentNode) === -1) {
+        arr.push(node.parentNode);
       }
-      return this;
-      // const api  = {....}
-      //return api;
-    },
-    find(selector) {
-      let arr = [];
-      for (let i = 0; i < elements.length; i++) {
-        arr = arr.concat(Array.from(elements[i].querySelectorAll(selector)));
+    });
+    return jQuery(arr);
+  },
+  children() {
+    const arr = [];
+    this.each((node) => {
+      if (arr.indexOf(node.children) === -1) {
+        arr.push(...node.children);
       }
-      arr.oldJQ = this;
-      //保存执行find操作的JQ对象（旧JQ对象）
-      return jQuery(arr);
-      //这里创建一个新的jQuery对象，因为不能跟执行find操作的jQuery对象是一样的
-      //如果不返回新的jQuery对象，可能会
-    },
-    each(fn) {
-      for (let i = 0; i < elements.length; i++) {
-        fn.call(null, elements[i], i);
+    });
+    return jQuery(arr);
+  },
+  siblings() {
+    const arr = [];
+    this.each((node) => {
+      for (let i = 0; i < node.parentNode.children.length; i++) {
+        if (node.parentNode.children[i] !== node) {
+          arr.push(node.parentNode.children[i]);
+        }
       }
-      return this;
-    },
-    parent() {
-      const arr = [];
-      this.each((node) => {
-        if (arr.indexOf(node.parentNode) === -1) {
-          arr.push(node.parentNode);
-        }
-      });
-      return jQuery(arr);
-    },
-    children() {
-      const arr = [];
-      this.each((node) => {
-        if (arr.indexOf(node.children) === -1) {
-          arr.push(...node.children);
-        }
-      });
-      return jQuery(arr);
-    },
-    siblings() {
-      const arr = [];
-      this.each((node) => {
-        for (let i = 0; i < node.parentNode.children.length; i++) {
-          if (node.parentNode.children[i] !== node) {
-            arr.push(node.parentNode.children[i]);
-          }
-        }
-      });
-      return jQuery(arr);
-    },
-    print() {
-      console.log(elements);
-    },
-    end() {
-      return this.oldJQ;
-      //返回旧的JQ对象。
-    },
-  };
+    });
+    return jQuery(arr);
+  },
+  print() {
+    console.log(this.elements);
+  },
+  end() {
+    return this.oldJQ;
+    //返回旧的JQ对象。
+  },
 };
--- dom-2/src/main.js
+++ dom-2/src/main.js
@@ -5,6 +5,6 @@ const api2 = api.find(".children").addClass("blue");
 api.addClass("green");
 const oldApi = api2.end().addClass("yellow");
 
-const x = jQuery(".children");
+const x = jQuery(".children1");
 
 x.siblings().print();
